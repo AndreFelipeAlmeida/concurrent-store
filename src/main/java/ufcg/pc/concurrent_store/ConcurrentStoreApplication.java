cd3cc7c34ea9472a560a32485bcdc0ae0aedc7d2
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2025-04-07 01:46:59 -0300

    chore: build project

---
+++ src/main/java/ufcg/pc/concurrent_store/ConcurrentStoreApplication.java
@@ -0,0 +1,13 @@
+package ufcg.pc.concurrent_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufcg/pc/concurrent_store/ConcurrentStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ufcg.pc.concurrent_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
